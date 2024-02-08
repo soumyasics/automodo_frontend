828bd33b294f2962d0110c41ad0dba641bb60a03
Aswin CA <caaswin2001@gmail.com>
2024-02-08 21:00:37 +0530

    login

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './Components/Common/Footer';
 import Navbar from './Components/Common/Navbar';
+import LoginMechanic from './Components/Mechanic/LoginMechanic';
 function App() {
   return (
     <div>
       <Navbar/>
+      <LoginMechanic/>
     </div>
   
   );
--- src/Components/Mechanic/LoginMechanic.js
+++ src/Components/Mechanic/LoginMechanic.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Signin_image from '../../Assets/signin_image.png'
+import './LoginMech.css'
 
 function LoginMechanic() {
   return (
-    <div>LoginMechanic</div>
+    <div>
+      <div>
+        <img className="loginMech-img"src={Signin_image} alt='Signin_image'/>
+      </div>
+    </div>
   )
 }
 
