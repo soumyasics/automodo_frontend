5743bccaec1ed4ee957225498b690f28cb725c14
robin-abraham-ta <robinabmrbn@gmail.com>
2024-02-08 20:58:34 +0530

    main page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './Components/Common/Footer';
 import Navbar from './Components/Common/Navbar';
+import MainAdmin from './Components/Admin/MainAdmin';
 function App() {
   return (
     <div>
-      <Navbar/>
+      {[<Navbar/>,<MainAdmin/>]}
     </div>
-  
   );
 }
 
