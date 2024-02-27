0f1d61542cd0e2937679ffe37ddf3d22aa126cab
robin-abraham-ta <robinabmrbn@gmail.com>
2024-02-28 01:31:51 +0530

    admin dashboard

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AdminTable from './Components/Admin/AdminTable';
 import SignupUser from './Components/User/SignupUser';
 import SignupWorkshop from './Components/Workshop/signupWorkshop'
+import AdminDashboard from './Components/Admin/AdminDashboard';
 function App() {
   return (
     <BrowserRouter>
@@ -24,7 +25,8 @@ function App() {
       <Route path='/workshop-login' element={[<Navbar/>,<LoginWorkshop/>,<Footer/>]}/> 
       <Route path='/user-signup' element={[<Navbar/>,<SignupUser/>,<Footer/>]}/>
       <Route path='/mechanic-signup' element={[<Navbar/>,<SignupMechanic/>,<Footer/>]}/>
-       <Route path='/workshop-signup' element={[<Navbar/>,<SignupWorkshop/>,<Footer/>]}/>
+      <Route path='/workshop-signup' element={[<Navbar/>,<SignupWorkshop/>,<Footer/>]}/>
+      <Route path='/admin-dash' element={[<Navbar/>,<AdminDashboard/>,<Footer/>]}/>
        
      </Routes>
      </BrowserRouter>
